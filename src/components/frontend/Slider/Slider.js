e8dfac6cf92b57f4c94d2e9095d77a989b4f171e
HoaDam2002 <damvanhoa305202@gmail.com>
2023-11-09 22:04:16 +0700

    search, location home

--- src/components/frontend/Slider/Slider.js
+++ src/components/frontend/Slider/Slider.js
@@ -1,13 +1,15 @@
+import SearchHorizontal from '~/components/frontend/SearchHorizontal/SearchHorizontal';
+
 function Slider() {
     return (
         <>
-            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleSlidesOnly" className="carousel slide position-relative" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item " data-bs-interval="4000">
                         <img
                             src="/image/slider/hoboi_slider.jpg"
                             className="d-block w-100"
-                            style={{ maxHeight: '450px' }}
+                            style={{ maxHeight: '450px', objectFit: 'cover' }}
                             alt="..."
                         />
                     </div>
@@ -15,11 +17,12 @@ function Slider() {
                         <img
                             src="/image/slider/khachsan_slider.jpg"
                             className="d-block w-100"
-                            style={{ maxHeight: '450px' }}
+                            style={{ maxHeight: '450px', objectFit: 'cover' }}
                             alt="..."
                         />
                     </div>
                 </div>
+                <SearchHorizontal />
             </div>
         </>
     );
--- src/pages/frontend/Home/Home.js
+++ src/pages/frontend/Home/Home.js
@@ -1,9 +1,96 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.css';
 import Slider from '~/components/frontend/Slider/Slider';
+import { Link } from 'react-router-dom';
 
+const cx = classNames.bind(styles);
 function Home() {
     return (
         <>
             <Slider />
+
+            {/* Location */}
+            <section className={cx('location')} style={{ margin: '100px 0 0 0' }}>
+                <div className="container">
+                    <h2 className={cx('location-content')}>Choose Your Location</h2>
+                    <div className={cx('row', 'location-box')}>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                            <strong className={cx('location-name')}>Đà Nẵng</strong>
+                            <div className={cx('location-image')}>
+                                <Link to={'/rooms'}>
+                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>Home</section>
         </>
     );
 }
