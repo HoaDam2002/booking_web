ed37048edc733adbd2a5dbb6ff13dac63524c097
HoaDam2002 <damvanhoa305202@gmail.com>
2023-11-09 23:01:24 +0700

    search  lần cuối

--- src/pages/frontend/Home/Home.js
+++ src/pages/frontend/Home/Home.js
@@ -14,75 +14,91 @@ function Home() {
                 <div className="container">
                     <h2 className={cx('location-content')}>Choose Your Location</h2>
                     <div className={cx('row', 'location-box')}>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
+                        <div className={cx('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-location')}>
                             <strong className={cx('location-name')}>Đà Nẵng</strong>
                             <div className={cx('location-image')}>
                                 <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
-                                </Link>
-                            </div>
-                        </div>
-                        <div className={cx('col-6', 'col-md-4', 'col-lg-3', 'card-location')}>
-                            <strong className={cx('location-name')}>Đà Nẵng</strong>
-                            <div className={cx('location-image')}>
-                                <Link to={'/rooms'}>
-                                    <img src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV" />
+                                    <img
+                                        src="https://lh4.googleusercontent.com/cr8jei9MZ0ySWxe9NeewExKUiJhnCCnIslFpM34ae_ASRSrqotH3aDzUkUBUWK-E6O16gaE3hajAC9dNu1SRhP14U9iiNhyMbfpf7q_gAao-boSVNM_w9b6BiTRRjd-roDpj6IRV"
+                                        alt="Đà Nẵng"
+                                    />
                                 </Link>
                             </div>
                         </div>
